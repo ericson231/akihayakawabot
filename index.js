61bdb4d8266dccaca08bbd61b41fc9594a4ef316
ericson231 <ericsonpascua899@gmail.com>
2024-07-25 19:27:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const logger = require("./utils/log.js");
 const path = require('path');
 const net = require('net');
 
-const PORT = 8080; // Use Heroku-provided port or default to 3000
+const process.env.PORT || 8080; // Use Heroku-provided port or default to 3000
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/includes/login/cover/index.html'));
